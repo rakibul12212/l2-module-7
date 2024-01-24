5d1ec5a67d430edea04cde76a8c6ad177208f017
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-01-25 04:22:20 +0600

    initialize Framer Motion

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Lesson1 from "./pages/Lesson1";
+
+const App = () => {
+  return (
+    <div>
+      <Lesson1></Lesson1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Lesson1.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Lesson1 = () => {
+  return (
+    <div>
+      <div className="size-4 bg-indigo-500 rounded-lg"></div>
+    </div>
+  );
+};
+
+export default Lesson1;
